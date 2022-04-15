6c1880ba6707722ecea57914d15dbf938bb5dbb2
pratikbhopal@gmail.com <praSMA@1>
2022-04-15 16:39:02 +0530

    initial commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import MarketingApp from './components/MarketingApp';
+import MarketinApp from './components/MarketingApp';
+
+export default () => {
+    return (<div><MarketingApp /></div>);
+}
