afaaad3b7ded80b57cc24fc2826bba8931bfb569
pratikbhopal@gmail.com <praSMA@1>
2022-04-16 12:01:22 +0530

    new header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import MarketingApp from './components/MarketingApp';
 import MarketinApp from './components/MarketingApp';
+import Header from './components/Header';
+import { BrowserRouter } from 'react-router-dom';
 
 export default () => {
-    return (<div><MarketingApp /></div>);
+    return (
+        <BrowserRouter>
+            <div>
+                <Header />
+                <MarketingApp />
+            </div>
+        </BrowserRouter>
+    );
 }
